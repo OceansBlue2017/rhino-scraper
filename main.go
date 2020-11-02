e212d4184ec255c07c3daee9a5ae4de111064082
OceansBlue2017 <OceansBluecode@gmail.com>
2020-11-01 20:15:19 -0600

    11012020

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ import (
 	"io/ioutil" // to be able to write to a file 
 )
 
-// adding a fake line here 
-
 type Fact struct {
 	ID          int    `json:"id"`
 	Description string `json:"description"`
